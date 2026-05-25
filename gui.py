9a7f675bef472562c263e0f2ff4dc5f1fbf4e051
Ramit72 <ramitgupta321@gmail.com>
2026-05-25 20:04:24 +0530

    Added updation to the text box and implemented error handling, and new Theme. Also added fix for Close button error

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkPurple4")
+label_time = sg.Text("", key="Time")
 label = sg.Text("Enter a Task: ")
 input_box = sg.InputText(tooltip="Enter a Task", key = "task")
 add_button = sg.Button("Add")
@@ -11,6 +14,7 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 window = sg.Window("To-Do App",
                     layout=[
+                        [label_time],
                         [label],
                         [input_box, add_button],
                         [list_box, edit_button, complete_button],
@@ -18,10 +22,11 @@ window = sg.Window("To-Do App",
                     ],
                     font=("Helvetica",12))
 while True:
-    event, value = window.read()
-    # print(1,event)
-    # print(2,value)
-    # print(3,value["tasks"][0])
+    event, value = window.read(timeout=1000)
+    if event == sg.WIN_CLOSED:
+        break
+    window["Time"].update(time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             tasks = functions.get_tasks()
@@ -29,31 +34,44 @@ while True:
             tasks.append(new_task)
             functions.write_tasks(tasks)
             window["tasks"].update(tasks)
+            window["task"].update("")
 
         case "Edit":
-            task_to_edit = value["tasks"][0]
-            new_task = value["task"] + "\n"
+            try:
+                task_to_edit = value["tasks"][0]
+                new_task = value["task"] + "\n"
 
-            tasks = functions.get_tasks()
-            index = tasks.index(task_to_edit)
-            tasks[index] = new_task
-            functions.write_tasks(tasks)
-            window["tasks"].update(tasks)
+                tasks = functions.get_tasks()
+                index = tasks.index(task_to_edit)
+                tasks[index] = new_task
+                functions.write_tasks(tasks)
+                window["tasks"].update(tasks)
+
+            except IndexError:
+                sg.popup("Select an Item first",
+                         font=("Helvetica", 12))
 
         case "Complete":
-            task_to_complete = value["tasks"][0]
-            tasks = functions.get_tasks()
-            tasks.remove(task_to_complete)
-            window["tasks"].update(tasks)
-            window["task"].update("")
-            functions.write_tasks(tasks)
+            try:
+                task_to_complete = value["tasks"][0]
+                tasks = functions.get_tasks()
+                tasks.remove(task_to_complete)
+                window["tasks"].update(tasks)
+                window["task"].update("")
+                functions.write_tasks(tasks)
+            except ValueError:
+                sg.popup("Select an Item first",
+                         font=("Helvetica", 12))
 
         case "Exit":
+            break
+
+        case sg.WIN_CLOSED:
             break
 
         case "tasks":
             window["task"].update(value=value["tasks"][0])
 
-        case sg.WIN_CLOSED:
-            break
+
+
 window.close()
